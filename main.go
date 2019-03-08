15ba1cec6e9701a684be38f1bb30324e8cc252ef
Dmitriy <webdev@Dmitriys-MacBook-Air.local>
2019-03-08 18:06:25 +0200

    Incapsulate reader logic

--- main.go
+++ main.go
@@ -10,46 +10,16 @@ func main() {
 
 	fmt.Println("Start process")
 
-	config := getConfig()
+	c := getConfig()
 
-	if config.Async {
-		processAsync(config)
-	} else {
-		processSync(config)
-
+	reader := Reader{
+		config:       c,
+		storage:      FileStorage{c},
+		communicator: DZoneCommunicator{c},
 	}
+	reader.read()
 
 	fmt.Println(time.Since(start).Seconds())
 
 }
 
-func processSync(c Config) {
-
-	fmt.Println("Syncroniouse request")
-
-	fs := FileStorage{c}
-	communicator := DZoneCommunicator{c}
-
-	rssReader := SyncRssReader{
-		config:       c,
-		communicator: communicator,
-		storage:      fs,
-	}
-
-	rssReader.read(c.Streams)
-}
-
-func processAsync(c Config) {
-
-	fmt.Println("Asyncroniouse request")
-
-	fs := FileStorage{c}
-	communicator := DZoneCommunicator{c}
-
-	rssReader := AsyncRssReader{
-		config:       c,
-		communicator: communicator,
-		storage:      fs,
-	}
-	rssReader.read(c.Streams)
-}
--- rss_reader.go
+++ rss_reader.go
@@ -1,5 +1,50 @@
 package main
 
+import "fmt"
+
 type RssReader interface {
 	read(map[string]RssStream)
+}
+
+type Reader struct {
+	config       Config
+	communicator Communicator
+	storage      Storage
+}
+
+func (r *Reader) read() {
+
+	var readScenario RssReader
+
+	if r.config.Async {
+		readScenario = r.asyncReader()
+	} else {
+		readScenario = r.syncReader()
+	}
+
+	readScenario.read(r.config.Streams)
+}
+
+func (r *Reader) syncReader() *SyncRssReader {
+
+	fmt.Println("Syncroniouse request")
+
+	return &SyncRssReader{
+		config:       r.config,
+		communicator: r.communicator,
+		storage:      r.storage,
+	}
+
+}
+
+func (r *Reader) asyncReader() *AsyncRssReader {
+
+	fmt.Println("Asyncroniouse request")
+
+	return &AsyncRssReader{
+		config:       r.config,
+		communicator: r.communicator,
+		storage:      r.storage,
+	}
+
 }
